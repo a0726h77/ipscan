509337fbe0056818501d6b8df1e7c9bd8fe120ed
yan <a0726h77@gmail.com>
2010-08-07 07:03:46 +0800

    layout prototype

---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# coding:utf-8
+
+import sys
+try:
+    import pygtk
+    pygtk.require("2.0")
+except:
+    pass
+try:
+    import gtk
+    import gtk.glade
+except:
+    sys.exit(1)
+
+class View:
+    def __init__(self):
+        # 載入 glade 檔
+        self.gladefile = "main.glade"
+        self.UI = gtk.glade.XML(self.gladefile)
+        # 載入視窗
+        self.window = self.UI.get_widget("window1")
+        # 載入元件
+        self.btn1 = self.UI.get_widget("buttton1")
+        # 顯示視窗畫面
+        self.window.show_all()
+
+#        # 設定事件
+#        dic = {"on_window1_destory" : gtk.main_quit,
+#        	"on_btn1_button_press_event" : self.on_btn1_button_press_event,
+#        	}
+#        # 連結事件
+#        self.UI.signal_autoconnect(dic)
+
+#    def on_btn1_button_press_event(self, widget, event):
+#        print "hello"
+
+def main():
+    gtk.main()
+
+if __name__ == "__main__":
+    tvexample = View()
+    main()
