bb2f46407e0f9af47cb8784bebff3cfb161c3ad5
yan <a0726h77@gmail.com>
2010-08-07 13:36:07 +0800

    use pure xxx.connect to coeenct signal, success !

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 # coding:utf-8
+#
+# A tool for ip test and scan
+
 
 import sys
 try:
@@ -21,16 +24,19 @@ class View:
         # 載入視窗
         self.window = self.UI.get_widget("window1")
         # 載入元件
-        self.btn1 = self.UI.get_widget("buttton1")
+        self.btn1 = self.UI.get_widget("button1")
         # 顯示視窗畫面
         self.window.show_all()
 
         # 設定事件
-        dic = {"on_window1_destory" : gtk.main_quit,
-        	"on_btn1_button_press_event" : self.on_btn1_button_press_event,
-        	}
+	#dic = {"on_window1_destory" : gtk.main_quit,
+	#	"on_btn1_button_press_event" : self.on_btn1_button_press_event,
+	#	}
         # 連結事件
-        self.UI.signal_autoconnect(dic)
+	#self.UI.signal_autoconnect(dic)
+
+	self.window.connect('destroy', gtk.main_quit) 
+        self.btn1.connect('clicked', self.on_btn1_button_press_event, None)
 
     def on_btn1_button_press_event(self, widget, event):
         print "hello"
